280d256c4fdeabc8ae85498f9b41a981e48e6f3c
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2020-10-03 21:11:54 +0900

    Update 5_linear_regression_ransac.py

--- 5_linear_regression_ransac.py
+++ 5_linear_regression_ransac.py
@@ -1,1 +1,78 @@
+import pandas as pd
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import numpy as np
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import RANSACRegressor
+
+df = pd.read_csv('https://raw.githubusercontent.com/rasbt/'
+                 'python-machine-learning-book-2nd-edition'
+                 '/master/code/ch10/housing.data.txt',
+                 header=None,
+                 sep='\s+')
+"""
+1. CRIM: 도시의 인당 범죄율
+2. ZN: 25,000 평방 피트가 넘는 주택 비율
+3. INDUS: 도시에서 소매 업종이 아닌 지역 비율
+4. CHAS: 찰스강 인접 여부(강 주변=1, 그 외=0)
+5. NOX: 일산화질소 농도(10ppm 당)
+6. RM: 주택의 평균 방 개수
+7. AGE: 1940년 이전에 지어진 자가 주택 비율
+8. DIS: 다섯 개의 보스턴 고용 센터까지 가중치가 적용된 거리
+9. RAD: 방사형으로 뻗은 고속도로까지 접근성 지수
+10. TAX: $10,000당 재산세율
+11. PTRATIO: 도시의 학생-교사 비율
+12. B: 1000(Bk - 0.63)^2, 여기에서 Bk는 도시의 아프리카계 미국인 비율
+13. LSTAT: 저소득 계층의 비율
+14. MEDV: 자가 주택의 중간 가격($1,000 단위)
+"""
+df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 
+              'NOX', 'RM', 'AGE', 'DIS', 'RAD', 
+              'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+
+
+# Linear Regression
+X = df[['RM']].values
+y = df['MEDV'].values
+
+ransac = RANSACRegressor(LinearRegression(),
+                         max_trials = 100,
+                         min_samples = 50,
+                         loss = 'absolute_loss', # 알고리즘이 학습한 직선과 샘플 포인트 간의 수직 거리의 절댓값 계산  
+                         residual_threshold = 5.0, # 학습한 직선과 수직 거리가 5 이내에 있는 정상 샘플만 포함
+                         random_state = 0)
+
+ransac.fit(X, y)
+
+# np.logical_not
+# https://m.blog.naver.com/PostView.nhn?blogId=wideeyed&logNo=221621793997&proxyReferer=https:%2F%2Fwww.google.com%2F
+inlier_mask = ransac.inlier_mask_
+outlier_mask = np.logical_not(inlier_mask)
+
+# print(line_X.shape) # (7, )
+# print(line_X[:, np.newaxis].shape) # (7, 1)
+# print(line_y_ransac.shape) # (7, )
+line_X = np.arange(3, 10, 1)
+line_y_ransac = ransac.predict(line_X[:, np.newaxis])
+
+
+# graph
+plt.scatter(X[inlier_mask], y[inlier_mask],
+            c='steelblue', edgecolor='white', 
+            marker='o', label='Inliers')
+plt.scatter(X[outlier_mask], y[outlier_mask],
+            c='limegreen', edgecolor='white', 
+            marker='s', label='Outliers')
+plt.plot(line_X, line_y_ransac, color='black', lw=2)   
+plt.xlabel('Average number of rooms [RM]')
+plt.ylabel('Price in $1000s [MEDV]')
+plt.legend(loc='upper left')
+
+plt.show()
+
+
+print('기울기: %.3f' % ransac.estimator_.coef_[0])
+print('절편: %.3f' % ransac.estimator_.intercept_)
