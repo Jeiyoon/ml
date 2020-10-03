c15ec8339db5fbdff3af0833c739f0287f0f5233
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2020-10-03 23:55:45 +0900

    Update 5_linear_regression_eval.py

--- 5_linear_regression_eval.py
+++ 5_linear_regression_eval.py
@@ -1,1 +1,83 @@
+import pandas as pd
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import numpy as np
+import scipy as sp
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import RANSACRegressor
+from sklearn.model_selection import train_test_split
+
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+
+df = pd.read_csv('https://raw.githubusercontent.com/rasbt/'
+                 'python-machine-learning-book-2nd-edition'
+                 '/master/code/ch10/housing.data.txt',
+                 header=None,
+                 sep='\s+')
+"""
+1. CRIM: 도시의 인당 범죄율
+2. ZN: 25,000 평방 피트가 넘는 주택 비율
+3. INDUS: 도시에서 소매 업종이 아닌 지역 비율
+4. CHAS: 찰스강 인접 여부(강 주변=1, 그 외=0)
+5. NOX: 일산화질소 농도(10ppm 당)
+6. RM: 주택의 평균 방 개수
+7. AGE: 1940년 이전에 지어진 자가 주택 비율
+8. DIS: 다섯 개의 보스턴 고용 센터까지 가중치가 적용된 거리
+9. RAD: 방사형으로 뻗은 고속도로까지 접근성 지수
+10. TAX: $10,000당 재산세율
+11. PTRATIO: 도시의 학생-교사 비율
+12. B: 1000(Bk - 0.63)^2, 여기에서 Bk는 도시의 아프리카계 미국인 비율
+13. LSTAT: 저소득 계층의 비율
+14. MEDV: 자가 주택의 중간 가격($1,000 단위)
+"""
+df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 
+              'NOX', 'RM', 'AGE', 'DIS', 'RAD', 
+              'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+
+
+# Linear Regression
+X = df.iloc[:, :-1].values
+y = df['MEDV'].values
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size = 0.3, random_state = 0
+)
+
+slr = LinearRegression()
+
+slr.fit(X_train, y_train)
+y_train_pred = slr.predict(X_train)
+y_test_pred = slr.predict(X_test)
+
+# print(ary)
+# print(ary.shape)
+ary = np.array(range(10000))
+
+# %timeit np.linalg.norm(ary)
+# %timeit sp.linalg.norm(ary)
+# %timeit np.sqrt(np.sum(ary**2))
+plt.scatter(y_train_pred,  y_train_pred - y_train,
+            c='steelblue', marker='o', edgecolor='white',
+            label='Training data')
+plt.scatter(y_test_pred,  y_test_pred - y_test,
+            c='limegreen', marker='s', edgecolor='white',
+            label='Test data')
+plt.xlabel('Predicted values')
+plt.ylabel('Residuals')
+plt.legend(loc='upper left')
+plt.hlines(y=0, xmin=-10, xmax=50, color='black', lw=2)
+plt.xlim([-10, 50])
+plt.tight_layout()
+
+plt.show()
+
+print('훈련 MSE: %.3f, 테스트 MSE: %.3f' % (
+        mean_squared_error(y_train, y_train_pred),
+        mean_squared_error(y_test, y_test_pred)))
+print('훈련 R^2: %.3f, 테스트 R^2: %.3f' % (
+        r2_score(y_train, y_train_pred),
+        r2_score(y_test, y_test_pred)))
